e875d4a3cb7fec9398d08e94b9d1934c5838ee09
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 20:43:31 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def hello(array)
   i = 0
-  while i < array.length
+  while i < array.length #return value of while loop always nil.
     yield(array[i])
     i += 1
   end
 end
 
 binding.pry  #call method
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
